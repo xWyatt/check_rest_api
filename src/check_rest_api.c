56a8c434dc1f2921f0aa35392f5f520fbb02c8f5
okrouhly <18015817+okrouhly@users.noreply.github.com>
2021-09-20 15:51:39 +0200

    Added -d for debug and -D for HTTP headers

--- src/check_rest_api.c
+++ src/check_rest_api.c
@@ -38,7 +38,8 @@ void end(int exitCode) {
   free(argVals->criticalMin);
   free(argVals->criticalMax);
   free(argVals->criticalInclusive);
-
+  free(argVals->header);
+  
   exit(exitCode);
 }
 
@@ -78,12 +79,89 @@ size_t write_data(void *buf, size_t size, size_t nmemb, void *userp) {
   return nmemb;
 }
 
+static
+void dump(const char *text,
+          FILE *stream, unsigned char *ptr, size_t size)
+{
+  size_t i;
+  size_t c;
+  unsigned int width=0x10;
+
+  fprintf(stream, "%s, %10.10ld bytes (0x%8.8lx)\n",
+          text, (long)size, (long)size);
+
+  for(i=0; i<size; i+= width) {
+    fprintf(stream, "%4.4lx: ", (long)i);
+
+    /* show hex to the left */
+    for(c = 0; c < width; c++) {
+      if(i+c < size)
+        fprintf(stream, "%02x ", ptr[i+c]);
+      else
+        fputs("   ", stream);
+    }
+
+    /* show data on the right */
+    for(c = 0; (c < width) && (i+c < size); c++) {
+      char x = (ptr[i+c] >= 0x20 && ptr[i+c] < 0x80) ? ptr[i+c] : '.';
+      fputc(x, stream);
+    }
+
+    fputc('\n', stream); /* newline */
+  }
+}
+
+static
+int my_trace(CURL *handle, curl_infotype type,
+             char *data, size_t size,
+             void *userp)
+{
+  const char *text;
+  (void)handle; /* prevent compiler warning */
+  (void)userp;
+
+  switch (type) {
+  case CURLINFO_TEXT:
+    fprintf(stderr, "== Info: %s", data);
+  default: /* in case a new one is introduced to shock us */
+    return 0;
+
+  case CURLINFO_HEADER_OUT:
+    text = "=> Send header";
+    break;
+  case CURLINFO_DATA_OUT:
+    text = "=> Send data";
+    break;
+  case CURLINFO_SSL_DATA_OUT:
+    text = "=> Send SSL data";
+    break;
+  case CURLINFO_HEADER_IN:
+    text = "<= Recv header";
+    break;
+  case CURLINFO_DATA_IN:
+    text = "<= Recv data";
+    break;
+  case CURLINFO_SSL_DATA_IN:
+    text = "<= Recv SSL data";
+    break;
+  }
+
+  dump(text, stderr, (unsigned char *)data, size);
+  return 0;
+}
+
 void* callAPI(void) {
 
   CURLcode res;
 
   curl = curl_easy_init();
   if (curl) {
+    if (argVals->debug) {
+      curl_easy_setopt(curl, CURLOPT_DEBUGFUNCTION, my_trace);
+      /* the DEBUGFUNCTION has no effect until we enable VERBOSE */
+      curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+    }
+
     curl_easy_setopt(curl, CURLOPT_URL, argVals->hostname);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);    
 
@@ -93,6 +171,13 @@ void* callAPI(void) {
       curl_easy_setopt(curl, CURLOPT_USERNAME, argVals->username);
       curl_easy_setopt(curl, CURLOPT_PASSWORD, argVals->password);
       curl_easy_setopt(curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    }
+
+    if (argVals->header != NULL) {
+      struct curl_slist *chunk = NULL;
+
+      chunk = curl_slist_append(chunk, argVals->header);
+      curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
     }
 
     if (argVals->insecureSSL == 1) {
