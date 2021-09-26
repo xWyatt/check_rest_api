0c16f361940b49dec972b53f499799587e94a48b
Wyatt Teeter <teeterwyatt@gmail.com>
2021-09-26 16:50:20 -0500

    Sends post parameters with a POST request

--- src/check_rest_api.c
+++ src/check_rest_api.c
@@ -187,6 +187,7 @@ void* callAPI(void) {
     // Check if we need to change our method from GET
     if (argVals->httpMethod == 1) {
       curl_easy_setopt(curl, CURLOPT_POST, 1L);
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, ""); // HTTP server might now allow POST requests without parameters
     } else if (argVals->httpMethod == 2) {
       curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
     }
